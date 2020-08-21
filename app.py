853f8eae5440e29d1d72f80db858dbc43b503d4d
Yeboster <yeboster@gmail.com>
2020-08-21 23:04:13 +0200

    Added Calendar wrapper and test notebook.

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from __future__ import print_function
-import datetime
-import pickle
+
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import pickle
+from datetime import date, datetime, timedelta
+from os import getenv
+from os.path import expanduser
 from typing import *
 
+from dotenv import load_dotenv
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def authenticate() -> Any:
+def authenticate():
     """Authenticate and return the api object"""
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -34,22 +39,54 @@ def authenticate() -> Any:
 
     return build('calendar', 'v3', credentials=creds)
 
-def upcoming_10_events(service):
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+# Unfortunately Google Calendar API is a dynamic object, so no intelli-sense
+class Calendar():
+    """A wrapper to interact with Google Calendar"""
+
+    def __init__(self, calendar_id: str, timezone: str = None) -> None:
+        self.service = authenticate()
+        self.id = calendar_id
+        self.timezone = timezone if timezone else "Europe/Rome"
+
+    def get_calendars(self) -> List[Dict[str, Any]]:
+        calendars = self.service.calendarList().list(  # pylint: disable=maybe-no-member
+        ).execute()
+
+        return calendars['items']
+
+    def insert_event(self, title: str, date: date = None, start_datetime: datetime = None, end_datetime: datetime = None, description: str = None):
+        body: Dict[str, Any] = {}
+        body['summary'] = title
+
+        if date:
+            body['start'] = dict(date=date.isoformat())
+            body['end'] = dict(date=date.isoformat())
+        elif start_datetime:
+            time_zone = 'Europe/Rome'
+            body['start'] = dict(
+                dateTime=start_datetime.isoformat(), timeZone=time_zone)
+            if end_datetime:
+                body['end'] = dict(
+                    dateTime=end_datetime.isoformat(), timeZone=time_zone)
+            else:
+                end_datetime = start_datetime + timedelta(hours=1)
+                body['end'] = dict(
+                    dateTime=end_datetime.isoformat(), timeZone=time_zone)
+        else:
+            raise Exception('No date was passed')
+
+        if description:
+            body['notes'] = description
+
+        self.service.events().insert(calendarId=self.id,  # pylint: disable=maybe-no-member
+                                     body=body).execute()
 
 
 if __name__ == '__main__':
-    service = authenticate()
-    upcoming_10_events(service)
+    load_dotenv()
+
+    CALENDAR_ID = getenv('CALENDAR_ID')
+    TIMEZONE = getenv('TIMEZONE')
+
+    calendar = Calendar(CALENDAR_ID, TIMEZONE)
---
+++ notebook.py
@@ -0,0 +1,34 @@
+# %%
+from dotenv import load_dotenv
+from os import getenv
+from datetime import datetime, date
+import app
+
+# %%
+load_dotenv()
+CALENDAR_ID = getenv('CALENDAR_ID')
+# %%
+calendar = app.Calendar(CALENDAR_ID)
+# %%
+# Get calendar list
+for cal in calendar.get_calendars():
+    summary = cal['summary']
+    print(summary)
+
+    if summary.lower() == "primary":
+        print(cal)
+
+# %%
+# Add a test event at 21/8/2020
+test_date = date(2020, 8, 21)
+calendar.insert_event('Test event', date=test_date,
+                      description="Test description")
+
+# %%
+# Add a test event at 21/8/2020 23:00 - 00:00
+test_startdate = datetime(2020, 8, 21, 23, 0, 0)
+test_enddate = datetime(2020, 8, 21, 0, 0, 0)
+calendar.insert_event('Test event', start_datetime=test_startdate, end_datetime=test_enddate,
+                      description="Test description")
+
+# %%
