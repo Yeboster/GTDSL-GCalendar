b0d9fd7041fc1f8ec0675623822b15c5f97dfa86
Yeboster <yeboster@gmail.com>
2020-08-22 13:59:34 +0200

    Added time repetition function.

--- app.py
+++ app.py
@@ -55,7 +55,13 @@ class Calendar():
 
         return calendars['items']
 
-    def insert_event(self, title: str, date: date = None, start_datetime: datetime = None, end_datetime: datetime = None, description: str = None) -> None:
+    def get_events(self) -> List[Dict[str, Any]]:
+        events = self.service.events().list().execute() # pylint: disable=maybe-no-member
+
+        return events['items']
+
+    def insert_event(self, title: str, *, date: date = None, start_datetime: datetime = None, end_datetime: datetime = None, description: str = None) -> None:
+        """Insert event into calendar"""
         body: Dict[str, Any] = {}
         body['summary'] = title
 
@@ -82,6 +88,31 @@ class Calendar():
         self.service.events().insert(calendarId=self.id,  # pylint: disable=maybe-no-member
                                      body=body).execute()
 
+    def insert_time_repetition_event(self, title: str, *, date: date = None, start_datetime: datetime = None, end_datetime: datetime = None, description: str = None) -> None:
+        """Insert into calendar event with repeatition of 1 hour, 1 day, 1 week, 1 month"""
+        time_repetition = [1,7,30]
+
+        recurring_dates: List[Tuple[Any,Any]] = []
+        if date:
+           recurring_dates += [(date + timedelta(days=d), None) for d in time_repetition]
+        elif start_datetime:
+            # TODO: Check if recurringId can be used, to create recurring events
+            def add_to_endtime(**kwargs):
+                end_datetime + timedelta(**kwargs) if end_datetime else None
+
+            recurring_dates += [(start_datetime, add_to_endtime(hours=1))]
+            recurring_dates += [(start_datetime + timedelta(days=d), add_to_endtime(days=d)) for d in time_repetition]
+        else:
+            raise Exception('No date was passed')
+
+        for start, end in recurring_dates:
+            if type(start) is date:
+                self.insert_event(title, date=start, description=description)
+            else:
+                self.insert_event(title, start_datetime=start, end_datetime=end, description=description)
+
+
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -89,4 +120,5 @@ if __name__ == '__main__':
     CALENDAR_ID = getenv('CALENDAR_ID')
     TIMEZONE = getenv('TIMEZONE')
 
-    calendar = Calendar(CALENDAR_ID, TIMEZONE)
+    if CALENDAR_ID:
+        calendar = Calendar(CALENDAR_ID, TIMEZONE)
--- notebook.py
+++ notebook.py
@@ -7,8 +7,11 @@ import app
 # %%
 load_dotenv()
 CALENDAR_ID = getenv('CALENDAR_ID')
+assert CALENDAR_ID is not None
+
 # %%
 calendar = app.Calendar(CALENDAR_ID)
+
 # %%
 # Get calendar list
 for cal in calendar.get_calendars():
@@ -18,17 +21,23 @@ for cal in calendar.get_calendars():
     if summary.lower() == "primary":
         print(cal)
 
-# %%
-# Add a test event at 21/8/2020
-test_date = date(2020, 8, 21)
-calendar.insert_event('Test event', date=test_date,
-                      description="Test description")
+# # %%
+# # Add a test event at 21/8/2020
+# test_date = date(2020, 8, 21)
+# calendar.insert_event('Test event', date=test_date,
+#                       description="Test description")
+
+# # %%
+# # Add a test event at 21/8/2020 23:00 - 00:00
+# test_startdate = datetime(2020, 8, 21, 23, 0, 0)
+# test_enddate = datetime(2020, 8, 21, 0, 0, 0)
+# calendar.insert_event('Test event', start_datetime=test_startdate, end_datetime=test_enddate,
+#                       description="Test description")
 
 # %%
-# Add a test event at 21/8/2020 23:00 - 00:00
-test_startdate = datetime(2020, 8, 21, 23, 0, 0)
-test_enddate = datetime(2020, 8, 21, 0, 0, 0)
-calendar.insert_event('Test event', start_datetime=test_startdate, end_datetime=test_enddate,
-                      description="Test description")
+print("Add time repetition recurring event")
 
-# %%
+test_startdate = datetime(2020, 8, 22, 15, 0, 0)
+test_enddate = datetime(2020, 8, 22, 15, 30, 0)
+calendar.insert_time_repetition_event('Test event', start_datetime=test_startdate, end_datetime=test_enddate,
+                      description="Test description")
